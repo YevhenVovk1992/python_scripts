8d0d9bbe36dcbc5bbefe74194a083eaf0f3cce6c
Yevhen <104986485+YevhenVovk1992@users.noreply.github.com>
2022-06-02 17:21:39 +0300

    class inheritance
    I made a program that creates a new class that inherits the class lists and adds a new method to it.

---
+++ class_inheritance.py
@@ -0,0 +1,29 @@
+class ExtendedStack(list):
+    def sum(self):
+        # операция сложения
+        top1 = self.pop()
+        top2 = self.pop()
+        return self.append(top1 + top2)
+    def sub(self):
+        # операция вычитания
+        top1 = self.pop()
+        top2 = self.pop()
+        return self.append(top1 - top2)
+    def mul(self):
+        # операция умножения
+        top1 = self.pop()
+        top2 = self.pop()
+        return self.append(top1*top2)
+    def div(self):
+        # операция целочисленного деления
+        top1 = self.pop()
+        top2 = self.pop()
+        return self.append(top1//top2)
+
+q = ExtendedStack([1, 2, 4, 5, 6, 5, 5, 8])
+q.div()
+print('div', q)
+q.mul()
+print('mul', q)
+q.sub()
+print('sub', q)
