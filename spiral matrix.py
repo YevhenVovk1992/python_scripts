9496074a4d7fd641e43f88333fd43015997fe95c
Yevhen <104986485+YevhenVovk1992@users.noreply.github.com>
2022-06-28 16:46:59 +0300

    fix PEP8 errors and add comments

--- spiral matrix.py
+++ spiral matrix.py
@@ -1,23 +1,31 @@
-n = int(input('Введите размер матрицы: '))
-spiral = [[0] * n for _ in range(n)]
+n = int(input('Enter the matrix size: '))
 c = 1
+# Create array
+spiral = [[0] * n for _ in range(n)]
+
+# Fill in the matrix with the number
 for k in range(n //2 + 1):
     for j in range(k, n - k):
         if spiral[k][j] == 0:
             spiral[k][j] = c
             c += 1
+            
     for i in range(1 + k, n - k):
         if spiral[i][n - k - 1] == 0:
             spiral[i][n - k - 1] = c
             c += 1
+            
     for j in range(n - k - 2, k - 1, -1):
         if spiral[n - k - 1][j] == 0:
             spiral[n - k - 1][j] = c
             c += 1
+            
     for i in range(n - k - 2, k, -1):
         if spiral[i][k] == 0:
             spiral[i][k] = c
             c += 1
+            
+# Displaying the matrix            
 for i in range(n):
     for j in range(n):
         print(str(spiral[i][j]).ljust(3), end=' ')
